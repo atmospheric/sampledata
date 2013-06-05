a49a711d04d9d23e799db746dd74de83e8b03794
Chris Wynnyk <cwynnyk@mitre.org>
2013-06-05 12:03:24 -0400

    Added .ncx files to the gitignore.

---
+++ AtmoSampleData.m
@@ -0,0 +1,17 @@
+classdef AtmoSampleData
+  properties (Constant = true)
+    home = fileparts(which(mfilename));
+    
+    gfsHighresIsobaric = fullfile(AtmoSampleData.home,'gfs','gfsHighresIsobaric.grb2');
+    gfsLowresIsobaric = fullfile(AtmoSampleData.home,'gfs','gfsLowresIsobaric.grb2');
+    
+    namIsobaric = fullfile(AtmoSampleData.home,'nam','namIsobaric.grb2');
+    
+    rapHybrid13km = fullfile(AtmoSampleData.home,'rap','rapHybrid13km.grb2');
+    rapIsobaric13km = fullfile(AtmoSampleData.home,'rap','rapIsobaric13km.grb2');
+    
+    rucHybrid13km = fullfile(AtmoSampleData.home,'ruc','rucHybrid13km.grb2');
+    rucHybrid20km = fullfile(AtmoSampleData.home,'ruc','rucHybrid20km.grb2');
+    rucIsobaric13km = fullfile(AtmoSampleData.home,'ruc','rucHybrid13km.grb2');    
+  end
+end
